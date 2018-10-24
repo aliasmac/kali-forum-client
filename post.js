6236457e963d1109547b70931f05e46160d9cffd
thexyman <makdaa1@gmail.com>
2018-10-24 21:08:10 +0100

    finsihed core forum functionality

--- post.js
+++ post.js
@@ -7,6 +7,7 @@ class Post {
         this.media_element = postData.media_element
         this.author_id = postData.author_id
         this.comments = postData.comments
+        this.score = postData.score
     }
 
     renderAllComments () {
@@ -17,7 +18,7 @@ class Post {
         
         for (const el of comments) {
             const commentor = User.findById(parseInt(el.user_id))   
-            allComments.push(`<p><strong>${commentor.name}</strong> - ${el.comment}</p>`) 
+            allComments.push(`<p><strong>${commentor.name}</strong> - ${el.content}</p>`) 
         }   
 
         return allComments
@@ -33,11 +34,17 @@ class Post {
         postEl.className = 'post-panel'
         postEl.innerHTML = `
             <div id="post-id-${this.id}">
+
+            <div>
+                <span class="hvr-icon-bob" id=upvote-${this.id}><i class="fas fa-chevron-up hvr-icon" class="hvr-icon-bob" style="color: black;"></i></span>
+                <p id="score-${this.id}">${this.score}<p>
+                <span class="hvr-icon-bob" id=downvote-${this.id}><i class="fas fa-chevron-down hvr-icon" class="hvr-icon-bob" style="color: black;"></i></span>
+            </div>    
+
                 <h3>Feeling: ${this.title}</h3>
-                <p>The lowdown: ${this.content}</P>
+                <p id="lowdown-${this.id}">The lowdown: ${this.content}</P>
                 <div>
-                    <img 
-                    ${this.media_element}
+                    <img src="${this.media_element}" >
                 </div>
                 <p>Posted by: ${postAuthor.name}, from ${postAuthor.location}</p> 
 
@@ -45,11 +52,11 @@ class Post {
                 <button id="edit-btn-${this.id}" class="edit-btn">Edit your post</button>
             
                 <div class="accordion-content" id="accordion-${this.id}">
-                    <div>${this.renderAllComments()}</div>
+                    <div id=comment-container-${this.id}>${this.renderAllComments()}</div>
                     <div>
                         <form id="leave-comment-form-${this.id}">
                             <p>Add your own comment!</p>
-                            <select id="leave-comment-select">
+                            <select id="leave-comment-select-${this.id}">
                             </select>
                             <input type="text" id="title-input-${this.id}" ></input> 
                             <input type="submit" value="Add comment">
@@ -58,19 +65,32 @@ class Post {
                 </div>
 
                 <div class="edit-form" id="edit-${this.id}" >
+
                     <form id="edit-form-${this.id}">
-                        <input type="text" class="form-control" id="title-input-${this.id}" placeholder="${this.title}"></input> 
-                        <input type="text" class="form-control" id="feeling-input-${this.id}" placeholder="${this.content}"></input>       
-                        <input type="text" class="form-control" id="media-input-${this.id}" placeholder="${this.media_element}"></input>  
+                        <div>
+                            Title:
+                            <input type="text" id="edit-title-input-${this.id}" placeholder="${this.title}"></input>
+                        </div>
+                        <div>
+                            Feeling: 
+                            <input type="text" id="feeling-input-${this.id}" placeholder="${this.content}"></input>
+                        </div>
+                        <div>
+                            Gif:       
+                            <input type="text" id="media-input-${this.id}" placeholder="${this.media_element}"></input>  
+                        </div>
                         <input type="submit" value="Edit comment">
                     </form>
+
                 </div>
+
+
             </div>   
         `
         return postEl
     }
 
-          
+
 
 }
 
